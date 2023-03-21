1301acb890e162c91e9da1082b8fe329e24b53c1
Kartavchenko <stranger1301078@gmail.com>
2023-03-21 21:26:10 +0100

    added readme

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,3 +1,5 @@
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
 export default (async function sendResults(values) {
 
   const options = {
@@ -10,6 +12,11 @@ export default (async function sendResults(values) {
 
   try {
     const result = await fetch("https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/", options);
+    Report.success(
+      'Success',
+      'Your form was sent',
+      'Okay',
+    );
     return await result.json();
   } catch (error) {
     console.log(error)
